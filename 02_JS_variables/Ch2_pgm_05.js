bc767aaa20b32067087f6311e52c64bd91b95144
vijitha131 <163647702+vijitha131@users.noreply.github.com>
2024-07-14 16:32:50 +0530

    Update Ch2_pgm_05.js

--- 02_JS_variables/Ch2_pgm_05.js
+++ 02_JS_variables/Ch2_pgm_05.js
@@ -8,7 +8,14 @@ console.log(message);
 message = 'Congratulations! Your tweet has won a prize...';
 console.log(message);
 
+message = "This is your third message!";
+console.log(message);
 
+message = "Hello " + "World!";
+console.log(message);
+
+message = 'Congratulations!' + ' Your tweet' + ' has won a prize...';
+console.log(message);
 
 /* Further Adventures
  *
