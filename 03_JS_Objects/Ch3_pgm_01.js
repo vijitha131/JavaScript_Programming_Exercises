3bf3b562be8380d7c9157a477aa8fe646f976757
vijitha131 <163647702+vijitha131@users.noreply.github.com>
2024-07-17 12:20:10 +0530

    Update Ch3_pgm_01.js

--- 03_JS_Objects/Ch3_pgm_01.js
+++ 03_JS_Objects/Ch3_pgm_01.js
@@ -1,12 +1,44 @@
 // Using variables to represent a book
 
-var bookTitle;
-var bookAuthor;
+var bookTitle1;
+var bookAuthor1;
 
 bookTitle = "The Hobbit";
 bookAuthor = "J. R. R. Tolkien";
 
-console.log(bookTitle + " by " + bookAuthor);
+console.log(bookTitle1 + " by " + bookAuthor1);
+var bookTitle2;
+var bookAuthor2;
+
+bookTitle = "War and Peace";
+bookAuthor = "Leo Tolstoy";
+
+console.log(bookTitle2 + " by " + bookAuthor2);
+var bookTitle3;
+var bookAuthor3;
+
+bookTitle = "The Tempest";
+bookAuthor = "William Shakespeare";
+
+console.log(bookTitle3 + " by " + bookAuthor3);
+var books = [
+    { title: "The Hobbit", author: "J. R. R. Tolkien" },
+    { title: "1984", author: "George Orwell" },
+    { title: "To Kill a Mockingbird", author: "Harper Lee" },
+    { title: "Pride and Prejudice", author: "Jane Austen" },
+    { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+    { title: "Moby-Dick", author: "Herman Melville" },
+    { title: "War and Peace", author: "Leo Tolstoy" },
+    { title: "The Catcher in the Rye", author: "J. D. Salinger" },
+    { title: "The Lord of the Rings", author: "J. R. R. Tolkien" },
+    { title: "Harry Potter and the Philosopher's Stone", author: "J. K. Rowling" }
+];
+
+for (var i = 0; i < books.length; i++) {
+    console.log(books[i].title + " by " + books[i].author);
+}
+
+
 
 
 
