646724f4e66e2feeb2eb6245560326c62bdcdf77
vijitha131 <163647702+vijitha131@users.noreply.github.com>
2024-07-17 12:41:11 +0530

    Update Ch3_pgm_02.js

--- 03_JS_Objects/Ch3_pgm_02.js
+++ 03_JS_Objects/Ch3_pgm_02.js
@@ -9,10 +9,15 @@ var book2Author = "Philip Pullman";
 var book3Title = "The Adventures of Tom Sawyer";
 var book3Author = "Mark Twain";
 
-console.log("There are three books so far...");
+var book4Title = "Taming of the Shrew";
+var book4Author = "William Shakespeare";
+
+
+console.log("There are four books so far...");
 console.log(book1Title + " by " + book1Author);
 console.log(book2Title + " by " + book2Author);
 console.log(book3Title + " by " + book3Author);
+console.log(book4Title + " by " + book4Author);
 
 
 
